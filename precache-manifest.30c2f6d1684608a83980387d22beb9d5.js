940839351c01fe5dbd8afdd0c46b4a5ead49e59c
ItsNamrathaB <namrathabhere@gmail.com>
2021-08-13 01:02:04 +0530

    Updates

---
+++ precache-manifest.30c2f6d1684608a83980387d22beb9d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df73946469ef44ddda5a02efbd19ae73",
+    "url": "/Ta-Da-Tasks/index.html"
+  },
+  {
+    "revision": "27894e737e93156e42f2",
+    "url": "/Ta-Da-Tasks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a242f4cc294ae919b1a0",
+    "url": "/Ta-Da-Tasks/static/js/2.2fcd9e4f.chunk.js"
+  },
+  {
+    "revision": "70b070bc4c4b879d3d3b9683ea8bec9f",
+    "url": "/Ta-Da-Tasks/static/js/2.2fcd9e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27894e737e93156e42f2",
+    "url": "/Ta-Da-Tasks/static/js/main.fa6768db.chunk.js"
+  },
+  {
+    "revision": "961b42c06a9594efa53e",
+    "url": "/Ta-Da-Tasks/static/js/runtime-main.de0b3f34.js"
+  }
+]);
